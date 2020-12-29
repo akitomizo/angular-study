d41f299a3c2f7eaff7f80ea03fbf08dbc1151064
akitomizo <ak.mizo.112@gmail.com>
2020-12-29 18:45:43 +0900

    Initial commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+
+import { products } from "../products";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
+})
+export class ProductListComponent {
+  products = products;
+
+  items = ["iphone", "Android", "testaa"];
+
+  share() {
+    window.alert("The product has been shared!");
+  }
+
+  doSomething() {
+    window.alert("Test!");
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
