d9e39f1c6a711ab5f8aeb86fb9bd44a91fefd535
akitomizo <ak.mizo.112@gmail.com>
2020-12-29 22:41:35 +0900

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,23 @@ import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { AlertComponent } from "./alert/alert.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: "", component: ProductListComponent }])
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "detail", component: ProductDetailsComponent }
+    ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    AlertComponent
+    AlertComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [AppComponent]
 })
